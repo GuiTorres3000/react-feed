b06db4ddfbdfb7a38987856b056f13ae4e84b3c9
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-05-05 00:07:22 -0300

    refactor: post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,8 +2,21 @@ import style from './Post.module.css';
 
 export function Post() {
     return (
-        <dev className={style.post}>
+        <article className={style.post}>
+            <header>
+                {/*Autor com imagem e texto*/}
+                <div className={style}>
+                    <img className={style} src="https://github.com/GuiTorres3000.png"/>
+                    <div className={style}>
+                        <strong>Guilherme Towers</strong>
+                        <span>Web Dev - Full Stack</span>
+                    </div>
+
+                    
+                    <time title="05 de Maio às 20:30" dateTime="2025-05-05 08:20:32">Publicado a 10h</time>
+                </div>
+            </header>
             <h2>Post</h2>
-        </dev>
+        </article>
     )
 }
